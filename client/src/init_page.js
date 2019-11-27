17d13ac3851b8cd032ae8fb23ffa0070ddad7bc7
yangxingqiang <yangxingqiang@gridsum.com>
2019-11-27 15:47:16 +0800

    标注文档开关事件绑定以及页面布局

--- client/src/init_page.js
+++ client/src/init_page.js
@@ -8,7 +8,24 @@ var InitPage = (function ($, window, undefined) {
       $("#auth_form-ok").trigger("click");
     };
 
-    dispatcher.on('autoLogin', autoLogin);
+    /* 右上角的标准开关事件绑定 */
+    var switchChange = function () {
+      $("#brat-mark-switch").on('click', function () {
+        var _this = $(this);
+        var open = false
+        if (this.className === 'switch-wrap-off switch-off') { // 打开自动标注
+          open = true;
+          _this.removeClass('switch-wrap-off switch-off').addClass("switch-wrap-on switch-on");
+          _this.parent().removeClass('switch-wrap-off').addClass('switch-wrap-on');
+        } else {
+          _this.removeClass('switch-wrap-on switch-on').addClass("switch-wrap-off switch-off");
+          _this.parent().removeClass('switch-wrap-on').addClass('switch-wrap-off')
+        }
+        console.log(open ? "打开标注" : "关闭标注")
+      });
+    };
+
+    dispatcher.on('autoLogin', autoLogin).on('switchChange', switchChange);
   };
 
   return InitPage;
